4dfafcacb0e9f56f5d01af1715bf5c06047898ed
vmorhental <valera.morhental@gmail.com>
2023-04-29 10:20:57 +0300

    Homework dz-7 commit

---
+++ Homework-7/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+SolarSystem earth = SolarSystem.EARTH;
+        System.out.println("Distance from Sun is " + earth.getDistanceFromSun() + " light years");
+    }
+}
